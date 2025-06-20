58d383ea43f15006fe6c8e9ef44d38e858996271
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 01:20:38 +0200

    Abstract mapping logic into a dedicated object

--- Sources/MinimalAuthExample/Store/CodableUserStore.swift
+++ Sources/MinimalAuthExample/Store/CodableUserStore.swift
@@ -8,31 +8,36 @@ public class CodableUserStore {
         self.storeURL = storeURL
     }
     
-    private struct CodableStoredUser: Codable {
-        let id: UUID
-        let email: String
-    }
-    
     public func get() throws -> [User] {
         guard FileManager.default.fileExists(atPath: storeURL.path) else { return [] }
         let data = try Data(contentsOf: storeURL)
-        return try JSONDecoder().decode([CodableStoredUser].self, from: data).map {
-            User(id: $0.id, email: $0.email)
-        }
+        return try JSONDecoder().decode([CodableStoredUser].self, from: data).map(UserMapper.map)
     }
     
     public func saveUser(_ user: User) throws {
         var users = try get()
         users.append(user)
-        let mapped = users.map {
-            CodableStoredUser(id: $0.id, email: $0.email)
-        }
-        
-        let data = try JSONEncoder().encode(mapped)
+        let data = try JSONEncoder().encode(users.map(UserMapper.map))
         try data.write(to: storeURL)
     }
     
     public func findUser(byEmail email: String) throws -> User? {
        return try get().first { $0.email == email }
+    }
+}
+
+
+private struct CodableStoredUser: Codable {
+    let id: UUID
+    let email: String
+}
+
+private enum UserMapper {
+    static func map(_ user: CodableStoredUser) -> User {
+        User(id: user.id, email: user.email)
+    }
+    
+    static func map(_ user: User) -> CodableStoredUser {
+        CodableStoredUser(id: user.id, email: user.email)
     }
 }

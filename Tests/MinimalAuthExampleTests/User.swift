491172994dab7665caf68d8a6d198451151f57ab
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 01:12:06 +0200

    Move objects to their own file

---
+++ Tests/MinimalAuthExampleTests/User.swift
@@ -0,0 +1,6 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 21/6/25.
+import Foundation
+
+struct User: Equatable {
+        let id: UUID
+    }
--- Tests/MinimalAuthExampleTests/UserStoreTests.swift
+++ Tests/MinimalAuthExampleTests/UserStoreTests.swift
@@ -3,41 +3,6 @@
 import XCTest
 
 class UserStoreTests: XCTestCase {
-    
-    struct User: Equatable {
-        let id: UUID
-    }
-    
-    class CodableUserStore {
-        let storeURL: URL
-        init(storeURL: URL) {
-            self.storeURL = storeURL
-        }
-        
-        func get() throws -> [User] {
-            guard FileManager.default.fileExists(atPath: storeURL.path) else { return [] }
-            let data = try Data(contentsOf: storeURL)
-            return try JSONDecoder().decode([CodableStoredUser].self, from: data).map {
-                User(id: $0.id)
-            }
-        }
-        
-        private struct CodableStoredUser: Codable {
-            let id: UUID
-        }
-        
-        func saveUser(_ user: User) throws {
-            var users = try get()
-            users.append(user)
-            let mapped = users.map {
-                CodableStoredUser(id: $0.id)
-            }
-            
-            let data = try JSONEncoder().encode(mapped)
-            try data.write(to: storeURL)
-        }
-    }
-    
     override func setUp() {
         try? FileManager.default.removeItem(at: testSpecificURL())
     }

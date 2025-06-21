c54d4c90e692431db664c500aad49d7d3f92c03a
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 10:14:52 +0200

    Move auth doubles to a nested shared scope

---
+++ Tests/UseCases/Auth/AuthDoubles.swift
@@ -0,0 +1,45 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 21/6/25.
+
+
+import XCTest
+import MinimalAuthExample
+
+struct RecipeStoreDummy: RecipeStore {
+    func getRecipes() throws -> [Recipe] {
+        [ ]
+    }
+    
+    func createRecipe(userId: UUID, title: String) throws -> Recipe {
+        Recipe(id: UUID(), userId: userId, title: title)
+    }
+}
+
+class UserStoreSpy: UserStore {
+    private(set) var messages = [Message]()
+    
+    enum Message: Equatable {
+        case findUser(byEmail: String)
+        case saveUser(id: UUID, email: String, hashedPassword: String)
+    }
+    
+    func createUser(id: UUID, email: String, hashedPassword: String) throws {
+        messages.append(.saveUser(id: id, email: email, hashedPassword: hashedPassword))
+    }
+    
+    func findUser(byEmail email: String) throws -> User? {
+        messages.append(.findUser(byEmail: email))
+        return nil
+    }
+}
+
+struct UserStoreStub: UserStore {
+    let findUserResult: Result<User?, Error>
+    let saveResult: Result<Void, Error>
+    func findUser(byEmail email: String) throws -> User? {
+        try findUserResult.get()
+    }
+    
+    func createUser(id: UUID, email: String, hashedPassword: String) throws {
+        try saveResult.get()
+    }
+}
--- Tests/UseCases/Auth/LoginUseCaseTests.swift
+++ Tests/UseCases/Auth/LoginUseCaseTests.swift
@@ -90,43 +90,4 @@ class LoginUseCaseTests: XCTestCase {
     }
 }
 
-private extension LoginUseCaseTests {
-    struct RecipeStoreDummy: RecipeStore {
-        func getRecipes() throws -> [Recipe] {
-            [ ]
-        }
-        
-        func createRecipe(userId: UUID, title: String) throws -> Recipe {
-            Recipe(id: UUID(), userId: userId, title: title)
-        }
-    }
-    class UserStoreSpy: UserStore {
-        private(set) var messages = [Message]()
-        
-        enum Message: Equatable {
-            case findUser(byEmail: String)
-            case saveUser(id: UUID, email: String, hashedPassword: String)
-        }
-        
-        func createUser(id: UUID, email: String, hashedPassword: String) throws {
-            messages.append(.saveUser(id: id, email: email, hashedPassword: hashedPassword))
-        }
-        
-        func findUser(byEmail email: String) throws -> User? {
-            messages.append(.findUser(byEmail: email))
-            return nil
-        }
-    }
-    
-    struct UserStoreStub: UserStore {
-        let findUserResult: Result<User?, Error>
-        let saveResult: Result<Void, Error>
-        func findUser(byEmail email: String) throws -> User? {
-            try findUserResult.get()
-        }
-        
-        func createUser(id: UUID, email: String, hashedPassword: String) throws {
-            try saveResult.get()
-        }
-    }
-}
+

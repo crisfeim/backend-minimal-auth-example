832dec8acd4f68a21c80418fcaddd049fc5fb471
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 10:05:17 +0200

    Extract generic store to its own file

--- Sources/Store/CodableUserStore.swift
+++ Sources/Store/CodableUserStore.swift
@@ -2,26 +2,7 @@
 
 import Foundation
 
-final class CodableStore<T: Codable> {
-    let storeURL: URL
-    
-    init(storeURL: URL) {
-        self.storeURL = storeURL
-    }
-    
-    func save(_ object: T) throws {
-        var objects = try get()
-        objects.append(object)
-        let data = try JSONEncoder().encode(objects)
-        try data.write(to: storeURL)
-    }
-    
-    func get() throws -> [T] {
-        guard FileManager.default.fileExists(atPath: storeURL.path) else { return [] }
-        let data = try Data(contentsOf: storeURL)
-        return try JSONDecoder().decode([T].self, from: data)
-    }
-}
+
 
 
 public class CodableUserStore: UserStore {

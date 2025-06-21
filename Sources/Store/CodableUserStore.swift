913f4d8db49aa1b2942c09b78daf3af6df15b362
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 10:04:57 +0200

    Create generic codable store

--- Sources/Store/CodableUserStore.swift
+++ Sources/Store/CodableUserStore.swift
@@ -2,32 +2,44 @@
 
 import Foundation
 
-public class CodableUserStore: UserStore {
-    private let storeURL: URL
-    public init(storeURL: URL) {
+final class CodableStore<T: Codable> {
+    let storeURL: URL
+    
+    init(storeURL: URL) {
         self.storeURL = storeURL
     }
     
-    public func getUsers() throws -> [User] {
-        try get().map(UserMapper.map)
-    }
-    
-    public func createUser(id: UUID, email: String, hashedPassword: String) throws {
-        let user = CodableUser(id: id, email: email, hashedPassword: hashedPassword)
-        var users = try get()
-        users.append(user)
-        let data = try JSONEncoder().encode(users)
+    func save(_ object: T) throws {
+        var objects = try get()
+        objects.append(object)
+        let data = try JSONEncoder().encode(objects)
         try data.write(to: storeURL)
     }
     
-    public func findUser(byEmail email: String) throws -> User? {
-        return try get().first { $0.email == email }.map(UserMapper.map)
-    }
-    
-    private func get() throws -> [CodableUser] {
+    func get() throws -> [T] {
         guard FileManager.default.fileExists(atPath: storeURL.path) else { return [] }
         let data = try Data(contentsOf: storeURL)
-        return try JSONDecoder().decode([CodableUser].self, from: data)
+        return try JSONDecoder().decode([T].self, from: data)
+    }
+}
+
+
+public class CodableUserStore: UserStore {
+    private let store: CodableStore<CodableUser>
+    public init(storeURL: URL) {
+        self.store = .init(storeURL: storeURL)
+    }
+    
+    public func getUsers() throws -> [User] {
+        try store.get().map(UserMapper.map)
+    }
+    
+    public func createUser(id: UUID, email: String, hashedPassword: String) throws {
+        try store.save(CodableUser(id: id, email: email, hashedPassword: hashedPassword))
+    }
+    
+    public func findUser(byEmail email: String) throws -> User? {
+        return try store.get().first { $0.email == email }.map(UserMapper.map)
     }
 }
 

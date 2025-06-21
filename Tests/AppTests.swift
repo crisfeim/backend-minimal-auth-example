9ac5074bdb990d4b130c672a04671f22018740d6
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 06:07:04 +0200

    Add basic humming bird app test

---
+++ Tests/AppTests.swift
@@ -0,0 +1,16 @@
+@testable import MinimalAuthExample
+import Hummingbird
+import HummingbirdTesting
+import XCTest
+
+final class AppTests: XCTestCase {
+    func testApp() async throws {
+        let app = buildApplication(configuration: .init())
+        try await app.test(.router) { client in
+            try await client.execute(uri: "/", method: .get) { response in
+                XCTAssertEqual(response.status, .ok)
+                XCTAssertEqual(String(buffer: response.body), "Hello")
+            }
+        }
+    }
+}

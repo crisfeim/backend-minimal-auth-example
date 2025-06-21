f287b8600bf369f65a0120e4dfdd12df9ad98aa2
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 12:34:11 +0200

    Move stores to persistency folder within infrastructure folder

---
+++ Sources/Infrastructure/Persistency/CodableRecipeStore.swift
@@ -0,0 +1,45 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 21/6/25.
+
+import Foundation
+
+public class CodableRecipeStore: RecipeStore {
+    private let store: CodableStore<CodableRecipe>
+    
+    public init(storeURL: URL) {
+        self.store = .init(storeURL: storeURL)
+    }
+    
+    public func getRecipes() throws -> [Recipe] {
+        try store.get().map(RecipeMapper.map)
+    }
+    
+    public func createRecipe(userId: UUID, title: String) throws -> Recipe {
+        let recipe = CodableRecipe(id: UUID(), userId: userId, title: title)
+        try store.save(recipe)
+        return RecipeMapper.map(recipe)
+    }
+}
+
+private struct CodableRecipe: Codable {
+    let id: UUID
+    let userId: UUID
+    let title: String
+}
+
+private enum RecipeMapper {
+    static func map(_ recipe: Recipe) -> CodableRecipe {
+        CodableRecipe(
+            id: recipe.id,
+            userId: recipe.userId,
+            title: recipe.title
+        )
+    }
+    
+    static func map(_ recipe: CodableRecipe) -> Recipe {
+        Recipe(
+            id: recipe.id,
+            userId: recipe.userId,
+            title: recipe.title
+        )
+    }
+}

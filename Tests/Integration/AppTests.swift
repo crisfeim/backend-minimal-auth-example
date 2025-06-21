90bf04c49ad3a2bb9a6259bddbba2297814c154b
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 12:24:32 +0200

    Refacto

--- Tests/Integration/AppTests.swift
+++ Tests/Integration/AppTests.swift
@@ -3,7 +3,7 @@ import Hummingbird
 import HummingbirdTesting
 import XCTest
 
-final class AppTests: XCTestCase {
+final class AppTests: XCTestCase, @unchecked Sendable {
     
     override func setUp() {
         try? FileManager.default.removeItem(at: testSpecificURL())
@@ -16,71 +16,84 @@ final class AppTests: XCTestCase {
     func testApp() async throws {
         let userStoreURL = testSpecificURL().appendingPathComponent("users.json")
         let recipeStoreURL = testSpecificURL().appendingPathComponent("recipes.json")
-
+        
         let app = await makeApp(
             configuration: .init(),
             userStoreURL: userStoreURL,
             recipeStoreURL: recipeStoreURL
         )
         
-        let registerPayload = try bufferFrom(RegisterRequest(email: "hi@crisfe.im", password: "123456"))
-        let loginPayload = try bufferFrom(RegisterRequest(email: "hi@crisfe.im", password: "123456"))
-        
         try await app.test(.router) { client in
+            try await assertPostRegisterSucceeds(client, request: RegisterRequest(email: "hi@crisfe.im", password: "123456"))
             
-            try await client.execute(
-                uri: "/register",
-                method: .post,
-                headers: [.init("Content-Type")!: "application/json"],
-                body: registerPayload
-            ) { response in
-
-                let tokenResponse = try JSONDecoder().decode(TokenResponse.self, from: response.body)
-                XCTAssertFalse(tokenResponse.token.isEmpty)
-                XCTAssertEqual(response.status, .ok)
-            }
+            let token = try await assertPostLoginSucceeds(client, request: LoginRequest(email: "hi@crisfe.im", password: "123456"))
             
-            try await client.execute(
-                uri: "/login",
-                method: .post,
-                headers: [.init("Content-Type")!: "application/json"],
-                body: loginPayload
-            ) { response in
-
-                let tokenResponse = try JSONDecoder().decode(TokenResponse.self, from: response.body)
-                XCTAssertFalse(tokenResponse.token.isEmpty)
-                XCTAssertEqual(response.status, .ok)
-                
-                let postRecipePayLoad = try bufferFrom(CreateRecipeRequest(title: "Test recipe"))
-                
-                try await client.execute(
-                    uri: "/recipes",
-                    method: .post,
-                    headers: [
-                        .init("Content-Type")!: "application/json",
-                        .init("Authorization")!: "Bearer \(tokenResponse.token)"
-                    ],
-                    body: postRecipePayLoad
-                ) { response in
-                    let recipe = try JSONDecoder().decode(Recipe.self, from: response.body)
-                    XCTAssertEqual(recipe.title, "Test recipe")
-                    
-                    try await client.execute(
-                        uri: "/recipes",
-                        method: .get,
-                        headers: [
-                            .init("Content-Type")!: "application/json",
-                            .init("Authorization")!: "Bearer \(tokenResponse.token)"
-                        ]
-                    ) { response in
-                        let recipes = try JSONDecoder().decode([Recipe].self, from: response.body)
-                        XCTAssertEqual(recipes, [recipe])
-                    }
-                }
-            }
+            let recipe = try await assertPostRecipeSucceeds(client, accessToken: token, request: CreateRecipeRequest(title: "Test recipe"))
+            
+            let recipes = try await assertGetRecipesSucceeds(client, accessToken: token)
+            XCTAssertEqual(recipes, [recipe])
+            
         }
     }
+}
+    
+private extension AppTests {
+    func assertPostRegisterSucceeds(_ client: TestClientProtocol, request: RegisterRequest, file: StaticString = #filePath, line: UInt = #line) async throws {
+        try await client.execute(
+            uri: "/register",
+            method: .post,
+            headers: [.init("Content-Type")!: "application/json"],
+            body: try bufferFrom(request)
+        ) { response in
+            let tokenResponse = try JSONDecoder().decode(TokenResponse.self, from: response.body)
+            XCTAssertFalse(tokenResponse.token.isEmpty, file: file, line: line)
+            XCTAssertEqual(response.status, .ok, file: file, line: line)
+        }
+    }
+    
+    func assertPostLoginSucceeds(_ client: TestClientProtocol, request: LoginRequest, file: StaticString = #filePath, line: UInt = #line) async throws -> String {
+        try await client.execute(
+            uri: "/login",
+            method: .post,
+            headers: [.init("Content-Type")!: "application/json"],
+            body: try bufferFrom(request)
+        ) { response in
+            let tokenResponse = try JSONDecoder().decode(TokenResponse.self, from: response.body)
+            XCTAssertFalse(tokenResponse.token.isEmpty, file: file, line: line)
+            XCTAssertEqual(response.status, .ok, file: file, line: line)
+            return tokenResponse.token
+        }
+    }
+    
+    func assertPostRecipeSucceeds(_ client: TestClientProtocol, accessToken: String, request: CreateRecipeRequest, file: StaticString = #filePath, line: UInt = #line) async throws -> Recipe {
+        try await client.execute(
+            uri: "/recipes",
+            method: .post,
+            headers: [
+                .init("Content-Type")!: "application/json",
+                .init("Authorization")!: "Bearer \(accessToken)"
+            ],
+            body: try bufferFrom(request)
+        ) { response in
+            try JSONDecoder().decode(Recipe.self, from: response.body)
+        }
+    }
+    
+    func assertGetRecipesSucceeds(_ client: TestClientProtocol, accessToken: String, file: StaticString = #filePath, line: UInt = #line) async throws -> [Recipe] {
+        try await client.execute(
+            uri: "/recipes",
+            method: .get,
+            headers: [
+                .init("Content-Type")!: "application/json",
+                .init("Authorization")!: "Bearer \(accessToken)"
+            ]
+        ) { response in
+            return try JSONDecoder().decode([Recipe].self, from: response.body)
+        }
+    }
+}
 
+extension AppTests {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!

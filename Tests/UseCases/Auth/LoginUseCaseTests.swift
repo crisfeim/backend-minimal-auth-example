3ed419d0ed04cb9292bab9c7ec6edae93afa802f
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 09:19:17 +0200

    App.createRecipe delivers error on invalid access token

---
+++ Tests/UseCases/Auth/LoginUseCaseTests.swift
@@ -0,0 +1,132 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 21/6/25.
+
+import XCTest
+import MinimalAuthExample
+
+class LoginUseCaseTests: XCTestCase {
+   
+    func test_init_doesntMessagesStoreUponCreation() throws {
+        let store = UserStoreSpy()
+        let _ = makeSUT(store: store)
+        XCTAssertEqual(store.messages, [])
+    }
+    
+    func test_login_deliversErrorOnStoreError() async throws {
+        let store = UserStoreStub(findUserResult: .failure(anyError()), saveResult: .success(()))
+        let sut = makeSUT(store: store)
+        await XCTAssertThrowsErrorAsync(try await sut.login(email: "any-email", password: "any-password"))
+    }
+    
+    func test_login_deliversErrorOnNotFoundUser() async throws {
+        let store = UserStoreStub(findUserResult: .success(nil), saveResult: .success(()))
+        let sut = makeSUT(store: store)
+        await XCTAssertThrowsErrorAsync(try await sut.login(email: "any-email", password: "any-password")) { error in
+            XCTAssertTrue(error is RecipesApp.NotFoundUserError)
+        }
+    }
+    
+    func test_login_deliversErrorOnInvalidEmail() async throws {
+        let store = UserStoreStub(findUserResult: .success(anyUser()), saveResult: .success(()))
+        let sut = makeSUT(store: store, emailValidator: { _ in false })
+        await XCTAssertThrowsErrorAsync(try await sut.login(email: "any-email", password: "any-password")) { error in
+            XCTAssertTrue(error is RecipesApp.InvalidEmailError)
+        }
+    }
+    
+    func test_login_deliversErrorOnInvalidPassword() async throws {
+        let store = UserStoreStub(findUserResult: .success(anyUser()), saveResult: .success(()))
+        let sut = makeSUT(store: store, passwordValidator: { _ in false })
+        await XCTAssertThrowsErrorAsync(try await sut.login(email: "any-email", password: "any-password")) { error in
+            XCTAssertTrue(error is RecipesApp.InvalidPasswordError)
+        }
+    }
+    
+    func test_login_deliversErrorOnPasswordVerifierError() async throws {
+        let store = UserStoreStub(findUserResult: .success(anyUser()), saveResult: .success(()))
+        let sut = makeSUT(store: store, passwordVerifier: { _, _ in throw self.anyError() })
+        await XCTAssertThrowsErrorAsync(try await sut.login(email: "any-email", password: "any-password"))
+    }
+    
+    func test_login_deliversErrorOnIncorrectPassword() async throws {
+        let store = UserStoreStub(findUserResult: .success(anyUser()), saveResult: .success(()))
+        let sut = makeSUT(store: store, passwordVerifier: { _, _ in false })
+        await XCTAssertThrowsErrorAsync(try await sut.login(email: "any-email", password: "any-password")) { error in
+            XCTAssertTrue(error is RecipesApp.IncorrectPasswordError)
+        }
+    }
+    
+    func test_login_deliversProvidedTokenOnCorrectCredentialsAndFoundUser() async throws {
+        let store = UserStoreStub(findUserResult: .success(anyUser()), saveResult: .success(()))
+        let sut = makeSUT(store: store, tokenProvider: { _ in "any-provided-token" })
+        let token = try await sut.login(email: "any-email", password: "any-password")
+        XCTAssertEqual(token["token"], "any-provided-token")
+    }
+    
+    func makeSUT(
+        store: UserStore,
+        emailValidator: @escaping EmailValidator = { _ in true },
+        passwordValidator: @escaping PasswordValidator = { _ in true },
+        tokenProvider: @escaping AuthTokenProvider = { _ in "any-token" },
+        passwordVerifier: @escaping PasswordVerifier = { _,_ in true }
+    ) -> RecipesApp {
+        return RecipesApp(
+            userStore: store,
+            recipeStore: RecipeStoreDummy(),
+            emailValidator: emailValidator,
+            passwordValidator: passwordValidator,
+            tokenProvider: tokenProvider,
+            tokenVerifier: { _ in UUID() },
+            hasher: { $0 },
+            passwordVerifier: passwordVerifier
+        )
+    }
+    
+    func anyError() -> NSError {
+        NSError(domain: "any error", code: 0)
+    }
+    
+    func anyUser() -> User {
+        User(id: UUID(), email: "any-user@email.com", hashedPassword: "any-hashed-password")
+    }
+}
+
+private extension LoginUseCaseTests {
+    struct RecipeStoreDummy: RecipeStore {
+        func getRecipes() throws -> [Recipe] {
+            [ ]
+        }
+        
+        func createRecipe(userId: UUID, title: String) throws -> Recipe {
+            Recipe(id: UUID(), userId: userId, title: title)
+        }
+    }
+    class UserStoreSpy: UserStore {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case findUser(byEmail: String)
+            case saveUser(User)
+        }
+        
+        func saveUser(_ user: User) throws {
+            messages.append(.saveUser(user))
+        }
+        
+        func findUser(byEmail email: String) throws -> User? {
+            messages.append(.findUser(byEmail: email))
+            return nil
+        }
+    }
+    
+    struct UserStoreStub: UserStore {
+        let findUserResult: Result<User?, Error>
+        let saveResult: Result<Void, Error>
+        func findUser(byEmail email: String) throws -> User? {
+            try findUserResult.get()
+        }
+        
+        func saveUser(_ user: User) throws {
+            try saveResult.get()
+        }
+    }
+}
---
+++ Tests/UseCases/Auth/RegisterUseCaseTests.swift
@@ -0,0 +1,148 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 21/6/25.
+
+import XCTest
+import MinimalAuthExample
+
+class RegisterUseCaseTests: XCTestCase {
+    func test_init_doesntMessagesStoreUponCreation() throws {
+        let store = UserStoreSpy()
+        let _ = makeSUT(store: store)
+        XCTAssertEqual(store.messages, [])
+    }
+    
+    func test_register_deliversErrorOnStoreSaveError() async throws {
+        let store = UserStoreStub(
+            findUserResult: .success(anyUser()),
+            saveResult: .failure(anyError())
+        )
+        let sut = makeSUT(store: store)
+        await XCTAssertThrowsErrorAsync(try await sut.register(email: "any-email", password: "any-password"))
+    }
+    
+    func test_register_deliversErrorOnAlreadyExistingUser() async throws {
+        let store = UserStoreStub(
+            findUserResult: .success(anyUser()),
+            saveResult: .success(())
+        )
+        let sut = makeSUT(store: store)
+        await XCTAssertThrowsErrorAsync(try await sut.register(email: "any-email", password: "any-password")) { error in
+            XCTAssertTrue(error is RecipesApp.UserAlreadyExists)
+        }
+    }
+    
+    func test_register_deliversErrorOnInvalidEmail() async throws {
+        let store = UserStoreStub(
+            findUserResult: .success(nil),
+            saveResult: .success(())
+        )
+        let sut = makeSUT(store: store, emailValidator: { _ in false })
+        await XCTAssertThrowsErrorAsync(try await sut.register(email: "any-email", password: "any-password")) { error in
+            
+            XCTAssertTrue(error is RecipesApp.InvalidEmailError)
+        }
+    }
+    
+    func test_register_deliversErrorOnInvalidPassword() async throws {
+        let store = UserStoreStub(
+            findUserResult: .success(nil),
+            saveResult: .success(())
+        )
+        let sut = makeSUT(store: store, passwordValidator: { _ in false })
+        await XCTAssertThrowsErrorAsync(try await sut.register(email: "any-email", password: "any-password")) { error in
+            XCTAssertTrue(error is RecipesApp.InvalidPasswordError)
+        }
+    }
+    
+    func test_register_deliversProvidedTokenOnNewUserValidCredentialsAndUserStoreSuccess() async throws {
+        let store = UserStoreStub(
+            findUserResult: .success(nil),
+            saveResult: .success(())
+        )
+        
+        let sut = makeSUT(store: store, tokenProvider: { _ in "any-provided-token" })
+        let token = try await sut.register(email: "any-email", password: "any-password")
+        XCTAssertEqual(token["token"], "any-provided-token")
+    }
+    
+    func test_register_deliversErrorOnHasherError() async throws {
+        let store = UserStoreStub(
+            findUserResult: .success(nil),
+            saveResult: .success(())
+        )
+        let sut = makeSUT(store: store, hasher: { _ in throw self.anyError() })
+        await XCTAssertThrowsErrorAsync(try await sut.register(email: "any-email", password: "any-password"))
+    }
+    
+    func makeSUT(
+        store: UserStore,
+        emailValidator: @escaping EmailValidator = { _ in true },
+        passwordValidator: @escaping PasswordValidator = { _ in true },
+        tokenProvider: @escaping AuthTokenProvider = { $0 },
+        hasher: @escaping Hasher = { $0 }
+    ) -> RecipesApp {
+        return RecipesApp(
+            userStore: store,
+            recipeStore: RecipeStoreDummy(),
+            emailValidator: emailValidator,
+            passwordValidator: passwordValidator,
+            tokenProvider: tokenProvider,
+            tokenVerifier: { _ in UUID() },
+            hasher: hasher,
+            passwordVerifier: { _,_ in true }
+        )
+    }
+    
+    func anyError() -> NSError {
+        NSError(domain: "any error", code: 0)
+    }
+    
+    func anyUser() -> User {
+        User(id: UUID(), email: "any-user@email.com", hashedPassword: "any-hashed-password")
+    }
+}
+
+
+// MARK: - Double
+private extension RegisterUseCaseTests {
+    struct RecipeStoreDummy: RecipeStore {
+        func getRecipes() throws -> [Recipe] {
+            [ ]
+        }
+        
+        func createRecipe(userId: UUID, title: String) throws -> Recipe {
+            Recipe(id: UUID(), userId: userId, title: title)
+        }
+    }
+    
+    class UserStoreSpy: UserStore {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case findUser(byEmail: String)
+            case saveUser(User)
+        }
+        
+        func saveUser(_ user: User) throws {
+            messages.append(.saveUser(user))
+        }
+        
+        func findUser(byEmail email: String) throws -> User? {
+            messages.append(.findUser(byEmail: email))
+            return nil
+        }
+    }
+    
+    struct UserStoreStub: UserStore {
+        let findUserResult: Result<User?, Error>
+        let saveResult: Result<Void, Error>
+        func findUser(byEmail email: String) throws -> User? {
+            try findUserResult.get()
+        }
+        
+        func saveUser(_ user: User) throws {
+            try saveResult.get()
+        }
+    }
+}
+
+

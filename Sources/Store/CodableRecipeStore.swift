59a85e07c5d52b2858a3fe9cdab0b49de5e6d856
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 10:06:32 +0200

    Use generic store within codable recipe store

--- Sources/Store/CodableRecipeStore.swift
+++ Sources/Store/CodableRecipeStore.swift
@@ -3,29 +3,20 @@
 import Foundation
 
 public class CodableRecipeStore {
-    private let storeURL: URL
+    private let store: CodableStore<CodableRecipe>
     
     public init(storeURL: URL) {
-        self.storeURL = storeURL
+        self.store = .init(storeURL: storeURL)
     }
     
     public func getRecipes() throws -> [Recipe] {
-        try get().map(RecipeMapper.map)
+        try store.get().map(RecipeMapper.map)
     }
     
     public func createRecipe(userId: UUID, title: String) throws -> Recipe {
         let recipe = CodableRecipe(id: UUID(), userId: userId, title: title)
-        var recipes = try get()
-        recipes.append(recipe)
-        let data = try JSONEncoder().encode(recipes)
-        try data.write(to: storeURL)
+        try store.save(recipe)
         return RecipeMapper.map(recipe)
-    }
-    
-    private func get() throws -> [CodableRecipe] {
-        guard FileManager.default.fileExists(atPath: storeURL.path) else { return [] }
-        let data = try Data(contentsOf: storeURL)
-        return try JSONDecoder().decode([CodableRecipe].self, from: data)
     }
 }
 
@@ -34,7 +25,6 @@ private struct CodableRecipe: Codable {
     let userId: UUID
     let title: String
 }
-
 
 private enum RecipeMapper {
     static func map(_ recipe: Recipe) -> CodableRecipe {

1ef76567765c1276410070f4ebb06cd0e20dec03
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 09:31:01 +0200

    App.createRecipe creates recipe with user id obtained from token

--- Tests/UseCases/Recipes/CreateRecipesUseCaseTests.swift
+++ Tests/UseCases/Recipes/CreateRecipesUseCaseTests.swift
@@ -28,6 +28,18 @@ class CreateRecipesUseCaseTests: XCTestCase {
         XCTAssertEqual(recipe, stubbedRecipe)
     }
     
+    func test_postRecipe_createsRecipeWithUserIdFromToken() async throws {
+        let stubbedUserId = UUID()
+        let store = RecipeStoreSpy(result: .success(anyRecipe()))
+        let sut = makeSUT(store: store, tokenVerifier: { _ in stubbedUserId })
+        
+        let _ = try await sut.createRecipe(accessToken: "any valid access token", title: "Fried chicken")
+        
+        XCTAssertEqual(store.capturedMessages, [
+            .init(userId: stubbedUserId, title: "Fried chicken")
+        ])
+    }
+    
     func makeSUT(
         store: RecipeStore,
         tokenVerifier: @escaping AuthTokenVerifier = { _ in UUID() },
@@ -53,6 +65,29 @@ class CreateRecipesUseCaseTests: XCTestCase {
         
         func createRecipe(userId: UUID, title: String) throws -> Recipe {
             try result.get()
+        }
+    }
+    
+    class RecipeStoreSpy: RecipeStore {
+        let result: Result<Recipe, Error>
+        struct CreateRecipeCommand: Equatable {
+            let userId: UUID
+            let title: String
+        }
+        
+        var capturedMessages = [CreateRecipeCommand]()
+        
+        init(result: Result<Recipe, Error>) {
+            self.result = result
+        }
+        
+        func getRecipes() throws -> [Recipe] {
+            fatalError("should never be called within test case context")
+        }
+        
+        func createRecipe(userId: UUID, title: String) throws -> Recipe {
+            capturedMessages.append(.init(userId: userId, title: title))
+            return try result.get()
         }
     }
     

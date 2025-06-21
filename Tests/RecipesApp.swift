5d80ec803795a5060bedbf48e1140088e839f0d6
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 07:20:09 +0200

    Format

--- Tests/RecipesApp.swift
+++ Tests/RecipesApp.swift
@@ -8,41 +8,41 @@ typealias PasswordValidator = (_ password: String) -> Bool
 typealias AuthTokenProvider = (_ email: String) -> String
 
 class RecipesApp {
-        let store: UserStore
-        let emailValidator: EmailValidator
-        let passwordValidator: PasswordValidator
-        let tokenProvider: AuthTokenProvider
-        
-        init(store: UserStore, emailValidator: @escaping EmailValidator, passwordValidator: @escaping PasswordValidator, tokenProvider: @escaping AuthTokenProvider) {
-            self.store = store
-            self.emailValidator = emailValidator
-            self.passwordValidator = passwordValidator
-            self.tokenProvider = tokenProvider
-        }
-        
-        struct UserAlreadyExists: Error {}
-        struct InvalidEmailError: Error {}
-        struct InvalidPasswordError: Error {}
-        
-        func register(email: String, password: String) throws -> [String: String] {
-            guard try store.findUser(byEmail: email) == nil else {
-                throw UserAlreadyExists()
-            }
-            
-            guard emailValidator(email) else {
-                throw InvalidEmailError()
-            }
-            
-            guard passwordValidator(password) else {
-                throw InvalidPasswordError()
-            }
-            
-            try store.saveUser(User(id: UUID(), email: email, hashedPassword: password))
-            let token = tokenProvider(email)
-            return ["token": token]
-        }
+    let store: UserStore
+    let emailValidator: EmailValidator
+    let passwordValidator: PasswordValidator
+    let tokenProvider: AuthTokenProvider
     
-        func login(email: String, password: String) throws {
-            let _ = try store.findUser(byEmail: email)
-        }
+    init(store: UserStore, emailValidator: @escaping EmailValidator, passwordValidator: @escaping PasswordValidator, tokenProvider: @escaping AuthTokenProvider) {
+        self.store = store
+        self.emailValidator = emailValidator
+        self.passwordValidator = passwordValidator
+        self.tokenProvider = tokenProvider
     }
+    
+    struct UserAlreadyExists: Error {}
+    struct InvalidEmailError: Error {}
+    struct InvalidPasswordError: Error {}
+    
+    func register(email: String, password: String) throws -> [String: String] {
+        guard try store.findUser(byEmail: email) == nil else {
+            throw UserAlreadyExists()
+        }
+        
+        guard emailValidator(email) else {
+            throw InvalidEmailError()
+        }
+        
+        guard passwordValidator(password) else {
+            throw InvalidPasswordError()
+        }
+        
+        try store.saveUser(User(id: UUID(), email: email, hashedPassword: password))
+        let token = tokenProvider(email)
+        return ["token": token]
+    }
+    
+    func login(email: String, password: String) throws {
+        let _ = try store.findUser(byEmail: email)
+    }
+}

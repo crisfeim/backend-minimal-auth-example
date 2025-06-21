b676e0cfd7e48b7273ef744ca05c39e0570b48a2
Cristian Rojas <cristian.rojas@live.fr>
2025-06-21 07:00:29 +0200

    App.register delivers error on invalid password

--- Tests/RegisterUseCaseTests.swift
+++ Tests/RegisterUseCaseTests.swift
@@ -36,18 +36,23 @@ class RegisterUseCaseTests: XCTestCase {
     }
     
     typealias EmailValidator  = (_ email: String) -> Bool
+    typealias PasswordValidator = (_ password: String) -> Bool
     
     class RecipesApp {
         let store: UserStore
         let emailValidator: EmailValidator
+        let passwordValidator: PasswordValidator
         
-        init(store: UserStore, emailValidator: @escaping EmailValidator) {
+        init(store: UserStore, emailValidator: @escaping EmailValidator, passwordValidator: @escaping PasswordValidator) {
             self.store = store
             self.emailValidator = emailValidator
+            self.passwordValidator = passwordValidator
         }
         
         struct UserAlreadyExists: Error {}
         struct InvalidEmailError: Error {}
+        struct InvalidPasswordError: Error {}
+        
         func register(email: String, password: String) throws {
             guard try store.findUser(byEmail: email) == nil else {
                 throw UserAlreadyExists()
@@ -55,6 +60,10 @@ class RegisterUseCaseTests: XCTestCase {
             
             guard emailValidator(email) else {
                 throw InvalidEmailError()
+            }
+            
+            guard passwordValidator(password) else {
+                throw InvalidPasswordError()
             }
             
             try store.saveUser(User(id: UUID(), email: email, hashedPassword: password))
@@ -93,9 +102,26 @@ class RegisterUseCaseTests: XCTestCase {
         let sut = makeSUT(store: store, emailValidator: { _ in false })
         XCTAssertThrowsError(try sut.register(email: "any-email", password: "any-password"))
     }
+    
+    func test_register_deliversErrorOnInvalidPassword() throws {
+        let store = UserStoreStub(
+            findUserResult: .success(nil),
+            saveResult: .success(())
+        )
+        let sut = makeSUT(store: store, passwordValidator: { _ in false })
+        XCTAssertThrowsError(try sut.register(email: "any-email", password: "any-password"))
+    }
 
-    func makeSUT(store: UserStore, emailValidator: @escaping EmailValidator = { _ in true}) -> RecipesApp {
-        return RecipesApp(store: store, emailValidator: emailValidator)
+    func makeSUT(
+        store: UserStore,
+        emailValidator: @escaping EmailValidator = { _ in true },
+        passwordValidator: @escaping PasswordValidator = { _ in true }
+    ) -> RecipesApp {
+        return RecipesApp(
+            store: store,
+            emailValidator: emailValidator,
+            passwordValidator: passwordValidator
+        )
     }
     
     func anyError() -> NSError {
